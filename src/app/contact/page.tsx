8b3d99e9db9a09d24127b81ff5dc48aa684ff914
SecretCandy <parksw9762@gmail.com>
2025-01-27 00:14:22 +0900

    <update>:contact 페이지 기본 틀 작업 완료

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,26 @@
 function ContactPage() {
-  return <div>contactpage</div>;
+  return (
+    <div>
+      <div className="bg-green-300 h-[200px] flex flex-col items-center justify-center">
+        <div className="text-4xl font-bold mb-2">Contact</div>
+        <div className="mt-2">찾아오시는 길.</div>
+      </div>
+      <div className="max-w-4xl mx-auto">
+        <div className="flex justify-center">
+          <div className="text-2xl font-semibold mt-10">지도</div>
+        </div>
+        <div className="mt-8">
+          <div>
+            <div className="text-2xl font-semibold">주소</div>
+            <div>
+              [06978] <br />
+              서울특별시 동작구 상도로 369, <br />
+              숭실대학교 학생회관 232호, 땅의사람들
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default ContactPage;

f473cfc7b7e2c7777abb2a8add72ee8ba047e3fd
SecretCandy <parksw9762@gmail.com>
2025-02-17 03:09:31 +0900

    <feat>:캘린더 페이지 기능 구현 완료

---
+++ src/components/CalendarPage/Title.tsx
@@ -0,0 +1,12 @@
+function Title() {
+  return (
+    <div className="bg-green-700 text-white h-[200px] flex flex-col items-center justify-center">
+      <div className="text-4xl font-bold mb-2">캘린더</div>
+      <div className="mt-2">
+        땅의사람들의 일정을 확인할 수 있는 캘린더입니다.
+      </div>
+    </div>
+  );
+}
+
+export default Title;

d2cdd943f5fba79fd72d7caf3cf37aca43684da9
SecretCandy <parksw9762@gmail.com>
2025-02-22 03:57:44 +0900

    <init>:동아리소개 페이지 생성 완료

---
+++ src/components/AboutPage/Title.tsx
@@ -0,0 +1,10 @@
+function Title() {
+  return (
+    <div className="bg-green-700 text-white h-[200px] flex flex-col items-center justify-center">
+      <div className="text-4xl font-bold mb-2">소개</div>
+      <div className="mt-2">땅의사람들에 대해 소개합니다.</div>
+    </div>
+  );
+}
+
+export default Title;

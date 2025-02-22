ff596c9d48364a404dda06f76d460aa00c9de88c
SecretCandy <parksw9762@gmail.com>
2025-02-22 18:24:19 +0900

    <refactor>:contact page title 컴포넌트 분리4

---
+++ src/components/ContactPage/Title.tsx
@@ -0,0 +1,10 @@
+function Title() {
+  return (
+    <div className="bg-green-300 h-[270px] flex flex-col items-center justify-end pb-[70px]">
+      <div className="text-4xl font-bold mb-2">Contact</div>
+      <div className="mt-2">찾아오시는 길.</div>
+    </div>
+  );
+}
+
+export default Title;

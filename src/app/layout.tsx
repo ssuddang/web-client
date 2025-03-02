2b696147af0494bc4544ce08632e52e5f6f73316
SecretCandy <parksw9762@gmail.com>
2025-03-02 21:47:22 +0900

    favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/Header/Header';
 
 export const metadata = {
   title: '땅의사람들',
+  description: '땅의사람들 관리자 전용 로그인 페이지입니다.',
   icons: {
     icon: '/favicon.ico',
   },

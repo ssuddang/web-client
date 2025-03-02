b44dc07abde39307c0ebe3c51a615d18afe969ca
SecretCandy <parksw9762@gmail.com>
2025-03-02 21:32:21 +0900

    favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import '@/app/globals.css';
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 
+export const metadata = {
+  title: '땅의사람들',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
+
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">

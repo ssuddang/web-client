472527ac1ceec25ddf688d4a87d6f3e28227cd0e
SecretCandy <parksw9762@gmail.com>
2025-08-22 02:33:08 +0900

    <update>:지원하기 내용 업데이트

--- src/components/JoinPage/Poster.tsx
+++ src/components/JoinPage/Poster.tsx
@@ -3,22 +3,22 @@ import Image from 'next/image';
 function Poster() {
   return (
     <div>
-      <div className="flex w-full items-center lg:px-[130px] px-[20px]">
-        <hr className="w-full border-t border-black" />
+      {/* <div className="flex w-full items-center lg:px-[130px] px-[20px]"> */}
+      {/* <hr className="w-full border-t border-black" />
         <div className="flex justify-center lg:w-[300px] w-[400px] font-semibold">
           이전 모집 공고
-        </div>
-        <hr className="w-full border-t border-black" />
-      </div>
+        </div> */}
+      {/* <hr className="w-full border-t border-black" /> */}
+      {/* </div> */}
       <div className="flex flex-col justify-center items-center">
         <Image
-          src="/join-image/recruit2025-1.jpg"
+          src="/join-image/recruit2025-2.jpg"
           className="lg:w-[500px] lg:h-[500px] mt-[30px] mb-[10px] px-[20px]"
           alt="지원 포스터"
           width={500}
           height={500}
         />
-        <div>- 땅의사람들 2025년 1학기 지원 포스터 -</div>
+        <div>- 땅의사람들 2025년 2학기 지원 포스터 -</div>
       </div>
     </div>
   );
--- src/components/JoinPage/Schedule.tsx
+++ src/components/JoinPage/Schedule.tsx
@@ -1,16 +1,16 @@
 function Schedule() {
   return (
     <div className="flex flex-col items-center justify-center mt-[80px]">
-      <div className="font-bold text-[25px]">2025년 1학기 모집기간</div>
+      <div className="font-bold text-[25px]">2025년 2학기 모집기간</div>
       <div className="flex flex-col lg:flex-row items-center px-auto mt-[40px]">
         <div className="mx-[50px] flex flex-col items-center lg:mt-0 bg-green-100 rounded-xl p-4 w-[300px] lg:bg-white">
-          <div className="font-semibold text-[20px]">02/24(월) ~ 03/07(금)</div>
+          <div className="font-semibold text-[20px]">08/18(월) ~ 08/31(일)</div>
           <div className="mt-[40px] text-[20px] bg-green-700 text-white px-[15px] py-[10px] rounded-2xl">
             서류 모집
           </div>
         </div>
         <div className="mx-[50px] flex flex-col items-center mt-[40px] lg:mt-0 bg-green-100 rounded-xl p-4 w-[300px] lg:bg-white">
-          <div className="font-semibold text-[20px]">03/10(월)</div>
+          <div className="font-semibold text-[20px]">09/05(금)</div>
           <div className="mt-[40px] text-[20px] bg-green-700 text-white px-[15px] py-[10px] rounded-2xl">
             최종 합격자 발표
           </div>
